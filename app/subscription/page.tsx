60b2ec87ef6e175cb015a0e5e624d9e3a0d79e77
victor47n <victor47n@gmail.com>
2024-11-08 14:34:50 -0300

    feat: ensure transactions and subscription pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-export default function SubscriptionPage() {
+export default async function SubscriptionPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return <Navbar />;
 }
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,21 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function TransactionsPage() {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
 
   return (
     <>
