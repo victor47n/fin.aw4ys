95daca39dc180fe3bca3dbcd4773290e231bdb11
victor47n <victor47n@gmail.com>
2024-11-08 16:36:10 -0300

    feat: add summary cards

---
+++ app/(home)/_components/summary-card.tsx
@@ -0,0 +1,40 @@
+import AddTransactionButton from "@/app/_components/add-transaction-button";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import { formatCurrency } from "@/app/_utils/formatCurrency";
+import type { ReactNode } from "react";
+
+interface SummaryCardProps {
+  icon: ReactNode;
+  title: string;
+  amount: number;
+  size?: "small" | "large";
+}
+
+export default function SummaryCard({
+  icon,
+  title,
+  amount,
+  size = "small",
+}: SummaryCardProps) {
+  return (
+    <Card>
+      <CardHeader className="flex-row items-center gap-4">
+        {icon}
+        <p
+          className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
+        >
+          {title}
+        </p>
+      </CardHeader>
+      <CardContent className="flex justify-between">
+        <p
+          className={`font-bold ${size === "small" ? "text-2xl font-bold" : "text-4xl"}`}
+        >
+          {formatCurrency(amount)}
+        </p>
+
+        {size === "large" && <AddTransactionButton />}
+      </CardContent>
+    </Card>
+  );
+}
---
+++ app/(home)/_components/summary-cards.tsx
@@ -0,0 +1,76 @@
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./summary-card";
+import { db } from "@/app/_lib/prisma";
+
+interface SummaryCardsProps {
+  month: string;
+}
+
+export default async function SummaryCards({ month }: SummaryCardsProps) {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: { amount: true },
+      })
+    )._sum?.amount,
+  );
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: { amount: true },
+      })
+    )._sum?.amount,
+  );
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesa"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
+  );
+}
